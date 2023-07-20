c7d19764eb851374cd65675b39a7cfd5db69f467
archanajyothy <archanajyothy@gmail.com>
2023-07-20 16:05:02 +0530

    Avatar changed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -70,7 +70,7 @@ const Add = () => {
           </Typography>
           <UserBox>
             <Avatar
-              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/38c9ad52-f3e7-4953-bef9-72168735abd0/d9w957g-ab3343c3-c54c-4250-a5f7-623aac16d45a.jpg/v1/fill/w_1192,h_670,q_70,strp/aslan_and_lucy_by_chysheryl_d9w957g-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD03NjgiLCJwYXRoIjoiXC9mXC8zOGM5YWQ1Mi1mM2U3LTQ5NTMtYmVmOS03MjE2ODczNWFiZDBcL2Q5dzk1N2ctYWIzMzQzYzMtYzU0Yy00MjUwLWE1ZjctNjIzYWFjMTZkNDVhLmpwZyIsIndpZHRoIjoiPD0xMzY2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.uOYZ75_1NKj-BRTlvn9fxZ3pLk-tA3XviUqJ2DA0klw"
               sx={{ width: 30, height: 30 }}
             />
             <Typography fontWeight={500} variant="span">
